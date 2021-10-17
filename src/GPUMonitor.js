21a69c5011a44f89dfb46a6dbad1df7b6c8abac2
Nikolas Tomastik <nikolas.tsk@gmail.com>
2021-10-17 20:16:38 +0200

    Added paralell optimalization

--- src/GPUMonitor.js
+++ src/GPUMonitor.js
@@ -29,25 +29,33 @@ class GPUMonitor {
 
         console.log(`GPUMonitor: watching for change`);
         this.interval = setInterval(async () => {
-            const gpuTemp = await this.gpuManager.getGpuTemp();
-            const isDotaRunning = await isProcessRunning('dota');
-            const isMinerRunning = await miner.isRunning();
+            const [gpuTemp, isDotaRunning, isMinerRunning] = await Promise.all([
+                this.gpuManager.getGpuTemp(),
+                isProcessRunning('dota'),
+                miner.isRunning()
+            ])
             console.log(`GPUMonitor: ${gpuTemp.actualWatt}W/${gpuTemp.maxWatt}W,  dota running ${isDotaRunning}, spectate ${this.isInSpectateMode}, miner running ${isMinerRunning}`);
 
             const gpuLimit = config.GPU_WATT_MINING_LIMIT + config.GPU_WATT_EPSILON;
             const isAboveLimit = gpuTemp.maxWatt > gpuLimit
             if (this.isInSpectateMode) {
                 if (isAboveLimit) {
-                    await miner.spawn();
-                    await this.gpuManager.setMiningProfile();
+                    await Promise.all([
+                        miner.spawn(),
+                        this.gpuManager.setMiningProfile()
+                    ]);
                 }
             } else {
                 if (isDotaRunning && gpuTemp.maxWatt < gpuLimit) {
-                    await miner.kill();
-                    await this.gpuManager.setGamingProfile();
+                    await Promise.all([
+                        miner.kill(),
+                        this.gpuManager.setGamingProfile()
+                    ]);
                 } else if (!isDotaRunning && isAboveLimit) {
-                    await miner.spawn();
-                    await this.gpuManager.setMiningProfile();
+                    await Promise.all([
+                        miner.spawn(),
+                        this.gpuManager.setMiningProfile()
+                    ]);
                 }
             }
         }, config.REFRESH_INTERVAL_TIME);
